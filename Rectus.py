8e51ed551b8c0fa5806a0e5cd12fe2c9d730e58e
Leo Aqua <88901298+Leo-Aqua@users.noreply.github.com>
2022-03-31 17:42:08 +0200

    Add files via upload

---
+++ Rectus.py
@@ -0,0 +1,31 @@
+from pickle import TRUE
+from threading import TIMEOUT_MAX
+from tkinter.font import BOLD
+import win32gui, win32api, keyboard, random #imports
+
+print("""
+8888888b.  8888888888  .d8888b.  88888888888 888     888  .d8888b.  
+888   Y88b 888        d88P  Y88b     888     888     888 d88P  Y88b 
+888    888 888        888    888     888     888     888 Y88b.      
+888   d88P 8888888    888            888     888     888  "Y888b.   
+8888888P"  888        888            888     888     888     "Y88b. 
+888 T88b   888        888    888     888     888     888       "888 
+888  T88b  888        Y88b  d88P     888     Y88b. .d88P Y88b  d88P 
+888   T88b 8888888888  "Y8888P"      888      "Y88888P"   "Y8888P"  
+""")
+
+
+# prints logo
+def f1():
+    while True:
+        try:
+            dc = win32gui.GetDC(0)
+            win32gui.Rectangle(dc, random.randint(1, 2000), random.randint(1, 2000), random.randint(1, 2000), random.randint(1, 2000))
+            if keyboard.is_pressed("ü"): #leave blank if you don't want to make it exitable
+                break
+        except:
+            True
+
+f1()
+while True: #keeps Terminal alive
+    True
